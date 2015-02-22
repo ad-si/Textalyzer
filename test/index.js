e13ae9475403bdb9e8e7958130095eb9c1c79b15
Adrian Sieber <mail@adriansieber.com>
2015-02-22 12:31:27 +0100

    Add tests

---
+++ test/index.js
@@ -0,0 +1,10 @@
+var fs = require('fs'),
+	path = require('path'),
+	text = fs.readFileSync(
+		path.resolve(__dirname, '../examples/1984.txt'),
+		'utf-8'
+	),
+	textalyzer = require('../index.js')
+
+
+console.log(textalyzer(text).getStats())
